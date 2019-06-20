64991e3ee75c173bd5f6bf600c7e34fd19005edb
Christina Labay <Christina.m.labay@gmail.com>
2019-06-19 19:32:21 -0500

    minor boiler plate updates

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 
 const app = express()
 
@@ -12,7 +12,11 @@ const morganOption = (NODE_ENV === 'production')
     : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+)
 app.use(helmet())
 
 app.get('/', (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const app = require('./app')
 const { PORT } = require('./config')
 
+app.get('/api/*', (req, res) => {
+    res.json({ok:true});
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
-})
+});
 
